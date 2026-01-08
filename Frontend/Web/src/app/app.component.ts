ed974e43bdeb1bc401016de578f4861c8807a2ef
GonzaloBJ <gonzalobarahonaj@gmail.com>
2026-01-08 14:14:47 -0300

    cambios en el navbar

--- Frontend/Web/src/app/app.component.ts
+++ Frontend/Web/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './features/home/pages/home.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MENU_ITEMS } from './core/config/menu.config';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Web';
+
+  menuItems = MENU_ITEMS;
 }
